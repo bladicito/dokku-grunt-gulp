ffe4874958095d57cc68570729c1e6ce8ed7892b
Bladimir Ardiles <bladi_sys@hotmail.com>
2016-10-28 14:02:57 +0200

    adds plugin descriptor

--- smile-fe-deploy.sh
+++ smile-fe-deploy.sh
@@ -1,1 +1,20 @@
 #!/usr/bin/env bash
+echo "-----> Smilemary frontendFiles will be build now..."
+echo "-----> Node Modules will be installed and afterwared grunt will be run"
+
+COMMAND=$(cat <<EOF
+cd "/app" &&
+new_path="\$(find / -name npm | tail -1 | xargs -I % sh -c "dirname %")" &&
+PATH="\$PATH:\$new_path" &&
+PATH="\$PATH:\$(npm bin)" &&
+npm install
+npm install grunt-cli &&
+grunt
+EOF
+)
+
+id=$(docker run -d $IMAGE /bin/bash -c "$COMMAND")
+docker attach $id
+test $(docker wait $id) -eq 0
+docker commit $id $IMAGE > /dev/null
+echo "-----> Smilemary frontendFiles were compiled"
