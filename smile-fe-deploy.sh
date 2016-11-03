4966259989ac6407f2f1ed87c862b80fadd04069
Bladimir Ardiles <bladi_sys@hotmail.com>
2016-11-03 08:56:25 +0100

    performs corrections on script

--- smile-fe-deploy.sh
+++ smile-fe-deploy.sh
@@ -1,15 +1,23 @@
-#!/usr/bin/env bash
-echo "-----> Smilemary frontendFiles will be build now..."
-echo "-----> Node Modules will be installed and afterwared grunt will be run"
+#!/bin/bash
 
+echo "-----> Smilemary frontendFiles will be build now..."
+echo "-----> Node Modules will be installed and afterwars grunt will be run"
+
+APP="$1"; IMAGE="dokku/$APP"
 COMMAND=$(cat <<EOF
+apt-get update -y &&
+apt-get install -y node npm &&
 cd "/app" &&
 new_path="\$(find / -name npm | tail -1 | xargs -I % sh -c "dirname %")" &&
 PATH="\$PATH:\$new_path" &&
 PATH="\$PATH:\$(npm bin)" &&
-npm install
-npm install grunt-cli &&
-grunt
+npm install &&
+npm install -g grunt-cli &&
+rm /usr/sbin/node &&
+ln -s /usr/bin/nodejs /usr/sbin/node &&
+echo "start grunt" &&
+grunt --verbose &&
+echo "end grunt"
 EOF
 )
 
